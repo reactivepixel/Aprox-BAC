1c042334152634af437c83d261afa5a55cbac756
Trichh <tylerrichard@Tylers-MacBook-Pro.local>
2016-04-19 20:31:47 -0400

    fixing dashboard file

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 
 import * as actionCreators from '../action_creators';
 
+import TempComponent from './TempComponent';
+import Bar from './Bar';
 import DataCollection from './DataCollection';
-
-
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 
 export default class Dashboard extends React.Component {
@@ -16,6 +16,8 @@ export default class Dashboard extends React.Component {
   render() {
     return <div>
       <section id="container">
+        <Bar bacPercent="35" bac=".06" numOfDrinks="4" />
+        <TempComponent {...this.props} dispTitle="derp" changeFilter={this.props.changeFilter} />
         <DataCollection {...this.props} changeFilter={this.props.changeFilter} />
       </section>
     </div>
@@ -26,7 +28,7 @@ export default class Dashboard extends React.Component {
 function mapStateToProps(state){
     return {
       items: state.get('items'),
-      filter:state.get('filter')
+      filter: state.get('filter')
     }
 }
 
